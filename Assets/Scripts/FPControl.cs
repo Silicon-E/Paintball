83a4393100b0b52f559804c11a6ba538be328b34
S <S>
2017-10-19 10:40:14 -0600

    Added crouching
    Also added some possible utility classes

--- Assets/Scripts/FPControl.cs
+++ Assets/Scripts/FPControl.cs
@@ -8,16 +8,20 @@ public class FPControl : MonoBehaviour {
 	public Controller control;
 	public Collider collider;
 	public Rigidbody physics;
+	public GameObject player = null;
+	public float height;
 	public float moveV;
 	public float maxMove;
 	public float jumpI;
 	public float jumpPortion;//Portion of horix movement converted to vertical movement
+	public float crouchSpeed;
 	public Camera camera;
-
+	public float camHeight;
 	public LayerMask onGroundMask;
 
 	private bool cursorEngaged = true;
-	public GameObject player = null;
+	private float crouchFactor = 1f;
+	private bool onGround = false;
 
 	void Start ()
 	{
@@ -79,7 +83,7 @@ public class FPControl : MonoBehaviour {
 
 		}else
 			Cursor.lockState = CursorLockMode.None;
-		camera.transform.position = player.transform.position;
+		camera.transform.position = player.transform.position+new Vector3(0,camHeight,0);
 	}
 
 	void FixedUpdate ()
@@ -89,60 +93,83 @@ public class FPControl : MonoBehaviour {
 			return;
 		}
 
-
+		Controller.input inp;
 		if(cursorEngaged)
 		{
-			Controller.input inp = control.GetInput();
-			Debug.Log(inp.mouse);
+			inp = control.GetInput();
+
+
 			//                 position                                          halfextents                           direction     rotation        maxDist  layerMask
-			RaycastHit hit;
-			Debug.DrawRay(player.transform.position, Vector3.down*(1.01f), Color.red);
-			if(Physics.SphereCast(player.transform.position, 0.5f, Vector3.down, out hit, 1.01f-0.5f, onGroundMask))
-			{
-				Vector3 horizMove = new Vector3(physics.velocity.x, 0f, physics.velocity.z);//X & Z movement
-				Vector3 moveVec = Quaternion.Euler(0,camera.transform.rotation.eulerAngles.y,0) * new Vector3(inp.move.x, 0f, inp.move.y);
 
-				if(moveVec != Vector3.zero)
-				{//Debug.Log("goin");
-					if(horizMove.magnitude<=maxMove)
-					{
-						collider.material.staticFriction = 0f;
-						collider.material.dynamicFriction = 0f;
-						physics.velocity = moveVec*moveV;
-					}else
-					{
-						collider.material.staticFriction = 0.5f;
-						collider.material.dynamicFriction = 0.5f;
-						if(Vector3.Dot(horizMove, moveVec)<1f)
-							physics.AddForce(moveVec*moveV, ForceMode.Acceleration);//Applies velocity input as acceleration (a cheap fix)
-					}
+		}else //end of if engaged
+		{
+			inp = new Controller.input();//Neutral controls
+			inp.move = Vector2.zero;
+			inp.mouse = Vector2.zero;
+			inp.jump = false;
+			inp.crouch = false;
+			inp.mouseL = false;
 
-					/*if(horizMove.magnitude<=maxMove || Vector3.Dot(horizMove, moveVec)<1f)
+			//collider.material.staticFriction = 0.5f; Taken care of below
+			//collider.material.dynamicFriction = 0.5f;
+		}
+		//Whether engaged or not
+		RaycastHit hit;
+		Debug.DrawRay(player.transform.position+new Vector3(0,(1-crouchFactor)*height*0.5f,0), Vector3.down*(height*crouchFactor*0.5f +0.01f), Color.red);
+		if(Physics.SphereCast(player.transform.position+new Vector3(0,(1-crouchFactor)*height*0.5f,0), 0.5f, Vector3.down, out hit, height*crouchFactor*0.5f +0.01f-0.5f, onGroundMask))
+		{
+			onGround = true;
+
+			Vector3 horizMove = new Vector3(physics.velocity.x, 0f, physics.velocity.z);//X & Z movement
+			Vector3 moveVec = Quaternion.Euler(0,camera.transform.rotation.eulerAngles.y,0) * new Vector3(inp.move.x, 0f, inp.move.y);
+			moveVec*=crouchFactor;//The more crouched (max at time of writing: 0.5f), the slower
+
+			if(moveVec != Vector3.zero)
+			{//Debug.Log("goin");
+				if(horizMove.magnitude<=maxMove)
+				{
+					collider.material.staticFriction = 0f;
+					collider.material.dynamicFriction = 0f;
+					physics.velocity = moveVec*moveV;
+				}else
+				{
+					collider.material.staticFriction = 0.5f;
+					collider.material.dynamicFriction = 0.5f;
+					if(Vector3.Dot(horizMove, moveVec)<1f)
+						physics.AddForce(moveVec*moveV, ForceMode.Acceleration);//Applies velocity input as acceleration (a cheap fix)
+				}
+
+				/*if(horizMove.magnitude<=maxMove || Vector3.Dot(horizMove, moveVec)<1f)
 					{
 						physics.AddForce(moveVec*moveV, ForceMode.Acceleration);
 					}*/
-				}else
-				{//Debug.Log("stoppin");
-					//physics.velocity = Vector3.zero;
-					collider.material.staticFriction = 10f;
-					collider.material.dynamicFriction = 10f;
-				}
-
-				if(inp.jump)
-				{
-					physics.AddForce((Vector3.up*(jumpI + horizMove.magnitude*jumpPortion))*physics.mass, ForceMode.Impulse);
-					Vector3 newV = new Vector3(physics.velocity.x*(1-jumpPortion), physics.velocity.y, physics.velocity.z*(1-jumpPortion));
-					physics.velocity = newV;
-				}
 			}else
-			{
-				collider.material.staticFriction = 0f;
-				collider.material.dynamicFriction = 0f;
+			{//Debug.Log("stoppin");
+				//physics.velocity = Vector3.zero;
+				collider.material.staticFriction = 10f;
+				collider.material.dynamicFriction = 10f;
 			}
-		}else //end of if engaged
+
+			if(inp.jump)
+			{
+				physics.AddForce((Vector3.up*(jumpI + horizMove.magnitude*jumpPortion))*physics.mass, ForceMode.Impulse);
+				Vector3 newV = new Vector3(physics.velocity.x*(1-jumpPortion), physics.velocity.y, physics.velocity.z*(1-jumpPortion));
+				physics.velocity = newV;
+			}
+		}else
 		{
-			collider.material.staticFriction = 0.5f;
-			collider.material.dynamicFriction = 0.5f;
+			onGround = false;
+
+			collider.material.staticFriction = 0f;
+			collider.material.dynamicFriction = 0f;
 		}
+
+		crouchFactor += (inp.crouch ?-1 :1)*crouchSpeed*Time.deltaTime;
+		crouchFactor = Mathf.Clamp(crouchFactor, 0.5f, 1f);
+		float dDist = ((CapsuleCollider)collider).center.y - ((CapsuleCollider)collider).height*0.5f;
+		((CapsuleCollider)collider).height = height*crouchFactor;
+		((CapsuleCollider)collider).center = new Vector3(0, (1-crouchFactor)*height*0.5f, 0);
+		dDist -= ((CapsuleCollider)collider).center.y - ((CapsuleCollider)collider).height*0.5f;
+		if(onGround) player.transform.position += new Vector3(0,dDist,0);//new Vector3(0,crouchSpeed*Time.deltaTime*height*0.5f *(inp.crouch ?-1 :1),0);
 	}
 }
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,11 @@
+﻿//Code from Unify Wiki; I don't know how it works.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : Singleton<Manager> {
+	protected Manager () {} // guarantee this will be always a singleton only - can't use the constructor!
+
+	public string myGlobalVar = "whatever";
+}
